6da2cdffc6c1a9c2dbcbfe0b129afa573565d9c4
Grichnak <marekdimitri@gmail.com>
2023-08-24 17:58:52 +0200

    express server

--- server.js
+++ server.js
@@ -1,7 +1,50 @@
 const http = require('http');
 const app = require('./app');
+
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+
+    }
+    if (port >= 0){
+        return port;
+    }
+    return false;
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
  
-app.set('port', process.env.PORT || 3000 );
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const adress = server.adress();
+    const bind = typeof adress === 'string' ? 'pipe' + adress : 'port' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + 'requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + 'is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+
 const server = http.createServer(app);
 
-server.listen (process.env.PORT || 3000);
+server.on('error, errorHandler');
+server.on('listening', () =>{
+    const adress = server.adress();
+    const bind = typeof adress === 'string' ? 'pipe' + adress : 'port' + port;
+    console.log('Listening on ' + bind);
+});
+
+server.listen (port);
