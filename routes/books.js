ceeb593394a57b33e175baafcbdf51171f41aa3b
Grichnak <marekdimitri@gmail.com>
2023-09-07 15:26:29 +0200

    correction controller

--- routes/books.js
+++ routes/books.js
@@ -5,13 +5,13 @@ const router = express.Router();
 const booksCtrl = require('../controllers/books');
 
 //Définition des routes :
-router.post('/', auth, multer, booksCtrl.createBook); //création de livre
-router.get('/', booksCtrl.getAllBooks); //liste de tous les livres
-router.get('/bestrating', booksCtrl.getBestBooks); //liste des meilleurs livres par notation
-router.get('/:id', booksCtrl.getOneBook); //obtention d'un livre par son ID
-router.put('/:id', auth, multer, booksCtrl.modifyBook); //modification d'un livre par son ID
-router.delete('/:id', auth, booksCtrl.deleteBook); //suppression d'un livre par son ID
-router.post('/:id/rating', auth, booksCtrl.ratingBook); //notation d'un livre par son ID
+router.post('/', auth, multer.imageUploader.single('image'), multer.imgResize, booksControllers.createBook);
+router.put('/:id', auth, multer.imageUploader.single('image'), multer.imgResize, booksControllers.modifyBook);
+router.delete('/:id', auth, booksControllers.deleteBook);
+router.get('/bestrating', booksControllers.getBestRatingBooks);
+router.get('/:id', booksControllers.getOneBook);
+router.get('/', booksControllers.getAllBooks);
+router.post('/:id/rating', auth, booksControllers.postRatingBook);
 
 //Exportation du routeur :
 module.exports = router;
